660ac2b6b6881b5f8d02838b412c84c2443839e5
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-30 16:16:22 +0530

    Make the strings double-quoted

--- heroku.sh
+++ heroku.sh
@@ -2,28 +2,28 @@
 # Host the YELLOWPAGIST app on Heroku
 
 
-read -p 'YP API Key: ' apikey
+read -p "YP API Key: " apikey
 echo
 
-echo '[*] Creating Heroku app...'
+echo "[*] Creating Heroku app..."
 heroku create
 echo
 
-echo '[*] Adding YP APIKey to the Environment Variables...'
+echo "[*] Adding YP APIKey to the Environment Variables..."
 heroku config:add YPAPIKey=$apikey 
 echo
 
-echo '[*] Pushing to Heroku Git repo...'
-url = $(git push heroku master | awk '/deployed to Heroku/ {print $(NF-3)}')
+echo "[*] Pushing to Heroku Git repo..."
+url=$(git push heroku master | awk '/deployed to Heroku/ {print $(NF-3)}')
 echo
 
-echo '[*] Adding Redistogo addon...'
+echo "[*] Adding Redistogo addon..."
 heroku addons:create redistogo:nano
 echo
 
-echo '[*] Starting the worker process...'
+echo "[*] Starting the worker process..."
 heroku ps:scale worker=1
 echo
 
-echo '[*] Completed.'
-echo '[*] You can access the web-app at: $url'
+echo "[*] Completed."
+echo "[*] You can access the web-app at: $url"
