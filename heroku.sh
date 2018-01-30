8a8863ad01b9b4eca20792129bca9599d9911d73
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-30 16:03:48 +0530

    Add script to host app on Heroku

---
+++ heroku.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Host the YELLOWPAGIST app on Heroku
+
+
+read -p 'YP API Key: ' apikey
+echo
+
+echo '[*] Creating Heroku app...'
+heroku create
+echo
+
+echo '[*] Adding YP APIKey to the Environment Variables...'
+heroku config:add YPAPIKey=$apikey 
+echo
+
+echo '[*] Pushing to Heroku Git repo...'
+url = $(git push heroku master | awk '/deployed to Heroku/ {print $(NF-3)}')
+echo
+
+echo '[*] Adding Redistogo addon...'
+heroku addons:create redistogo:nano
+echo
+
+echo '[*] Starting the worker process...'
+heroku ps:scale worker=1
+echo
+
+echo '[*] Completed.'
+echo '[*] You can access the web-app at: $url'
